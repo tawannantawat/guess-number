6216e8dc9c516b42891697c1653d841013d18815
tawannantawat <ynantawat_n@elearning.cmu.ac.th>
2020-01-21 01:41:16 +0700

    0

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ document.getElementById("submitguess").onclick = function(){
        
     var x = document.getElementById("guessField").value; 
 
-    if(guess<=5){
+    if(guess<=10){
         if(x > 100 || x < 1){
             document.querySelector('.result').textContent = "Please Enter Number Between 0 and 100"; 
         }else{
-            document.querySelector('.turn').textContent = 5 - guess + " left";
+            document.querySelector('.turn').textContent = 10 - guess + " left";
             if(x == y){     
                 document.querySelector('.result').textContent = "CONGRATULATIONS!!! YOU GUESSED IT RIGHT IN " + guess + " GUESS ";                   
                 guess = 12;
